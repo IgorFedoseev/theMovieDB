e6c4ca24123e267bde6efed3c3f1f2a6286adb46
Igor Fedoseev <ifedoseev.flagman@gmail.com>
2022-02-08 22:48:43 +0300

    hive basic methods

--- lib/lessons_examples/hive_example/hive_example_model.dart
+++ lib/lessons_examples/hive_example/hive_example_model.dart
@@ -8,7 +8,31 @@ class HiveExampleWidgetModel{
   }
   void doSome() async {
     var box = await Hive.openBox<dynamic>('testBox'); // открываем box (коллекцию) и присваиваем имя
-    final name = box.get('name') as String?;
+    await box.put('name', 'Ivan'); // записать данные (ключ, значение)
+    await box.put('age', 32);
+    await box.put('friends', ['Lexa', 'Dima', 'Vasya']);
+    final name = box.get('name') as String?; //получить данные по ключу
+    final age = box.get('age') as int?;
+    final friends = box.get('friends') as List<String>?;
+    final surname = box.get('surname', defaultValue: 'Petrov') as String; // значение по умолчанию
     print(name);
+    print(surname);
+    print(age);
+    print(friends);
+    // box.putAll({'key1': 'value1', 42: 'life'}); можно добавлять сразу несколько записей с ключем любого типа
+    // final index = await box.add('Anything else'); // добавляет значение без ключа и возвращает индекс
+    // final anythingElse = box.getAt(index); // и по этому индексу получаем значение
+    // print(anythingElse);
+
+    // await box.deleteAt(0); // удалить по индексу
+    // await box.delete('surname'); // удалить по ключу
+    // await box.deleteFromDisk(); // удалить всё
+
+    final boxValues = box.values.toList(); // все значения
+    print(boxValues);
+    final boxKeys = box.keys.toList(); // все ключи
+    print(boxKeys);
+
+    box.close(); // закрываем box, чтобы выгрузился из памяти
   }
 }
