fe19893714054b8f65c85eebcd585c013f429e59
Igor Fedoseev <ifedoseev.flagman@gmail.com>
2022-02-20 23:21:09 +0300

    requests done

--- lib/domain/api_client/api_client.dart
+++ lib/domain/api_client/api_client.dart
@@ -7,33 +7,57 @@ class ApiClient {
   static const _imageUrl = 'https://image.tmdb.org/t/p/w500';
   static const _apiKey = '5ed3e0cc6ddcfd2b94b5515b08fc9119';
 
-  Future <String> makeToken() async{
+  Future<String> makeToken() async {
     final url = Uri.parse('$_host/authentication/token/new?api_key=$_apiKey');
     final request = await _client.getUrl(url);
     final response = await request.close();
-      final json = await response
-          .transform(utf8.decoder)
-          .toList()
-          .then((value) => value.join())
-          .then((v) => jsonDecode(v) as Map<String, dynamic>);
-      final token = json['request_token'] as String;
-      return token;
+    final json = await response
+        .transform(utf8.decoder)
+        .toList()
+        .then((value) => value.join())
+        .then((v) => jsonDecode(v) as Map<String, dynamic>);
+    final token = json['request_token'] as String;
+    return token;
   }
 
+  Future<String> validateUser(
+      {required String userName,
+      required String password,
+      required String requestToken}) async {
+    final url = Uri.parse(
+        '$_host/authentication/token/validate_with_login?api_key=$_apiKey');
+    final Map<String, dynamic> parameters = {
+      'username': userName,
+      'password': password,
+      'request_token': requestToken
+    };
+    final request = await _client.postUrl(url);
+    request.headers.contentType = ContentType.json; // стандартный хэдер
+    request.write(jsonEncode(parameters));
+    final response = await request.close();
+    final json = await response
+        .transform(utf8.decoder)
+        .toList()
+        .then((value) => value.join())
+        .then((v) => jsonDecode(v) as Map<String, dynamic>);
+    final token = json['request_token'] as String;
+    return token;
+  }
 
-
-  // Future<List<Post>> getPost() async {
-  //   final url = Uri.parse('https://jsonplaceholder.typicode.com/posts');
-  //   final request = await _client.getUrl(url);
-  //   final response = await request.close();
-  //   final json = await response
-  //       .transform(utf8.decoder)
-  //       .toList()
-  //       .then((value) => value.join())
-  //       .then((v) => jsonDecode(v) as List<dynamic>);
-  //   final result = json.map((dynamic e) => Post.fromJson(e as Map<String,dynamic>)).toList();
-  //   return result;
-  // }
-
-
+  Future<String> makeSession({required String requestToken}) async {
+    final url = Uri.parse(
+        '$_host/authentication/session/new?api_key=$_apiKey');
+    final Map<String, dynamic> parameters = {'request_token': requestToken};
+    final request = await _client.postUrl(url);
+    request.headers.contentType = ContentType.json; // стандартный хэдер
+    request.write(jsonEncode(parameters));
+    final response = await request.close();
+    final json = await response
+        .transform(utf8.decoder)
+        .toList()
+        .then((value) => value.join())
+        .then((v) => jsonDecode(v) as Map<String, dynamic>);
+    final sessionId = json['session_id'] as String;
+    return sessionId;
+  }
 }
